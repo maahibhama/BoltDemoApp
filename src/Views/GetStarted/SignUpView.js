f1ffc38b67b40fe30a078fb4db4ae2838cd82b5f
Mahendra Bhama <maahibhama@gmail.com>
2019-01-21 15:00:34 +0530

    complete signup screen api calls

--- src/Views/GetStarted/SignUpView.js
+++ src/Views/GetStarted/SignUpView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, TextInput, TouchableHighlight, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TextInput, TouchableHighlight, AsyncStorage, Keyboard } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import PropTypes from "prop-types";
+
 import { Logo, Icons } from '../../common/Assets';
 import { Color } from '../../common/Colors';
 import BaseNavigationHeader from '../../CustomUI/navigation-header/BaseNavigationHeader';
@@ -8,24 +10,66 @@ import { AppFont } from '../../common/Fonts';
 import Routes from '../../Navigation/Routes';
 import ManageKeyboardScrollView from '../../common/ManageKeyboardScrollView';
 import { isLoginUser } from '../../common/Constants';
+import { isValidEmail, isValidPassword, setCurrentUser } from '../../common/Utility';
+import { signUpUser } from '../../API/APIController';
+import { User } from '../../Prototypes/User';
 
 export default class LoginView extends Component {
     static navigationOptions = {
         header: null
     }
 
+    static contextTypes = {
+        presentActivityIndicator: PropTypes.func.isRequired,
+        dismissActivityIndicator: PropTypes.func.isRequired,
+        showAlert: PropTypes.func.isRequired
+    };
+
     state = {
-        isShowingPassword: false
+        isShowingPassword: false,
+        username: "",
+        email: "",
+        password: "",
     }
 
-    logInButtonAction() {
-        AsyncStorage.setItem(isLoginUser, "true").then(()=>{
-            this.props.navigation.navigate(Routes.AppNavigator);
-        }) 
+    signInButtonAction() {
+        this.props.navigation.navigate(Routes.Login);
     }
 
     signUpButtonAction() {
-        this.props.navigation.navigate(Routes.Login);
+        Keyboard.dismiss()
+
+        var messageString = ''
+        if (this.state.username.length == 0) {
+            messageString = "Please enter name"
+        }
+        else if (isValidEmail(this.state.email) === false) {
+            messageString = "Invalid Email Format"
+        }
+        else if (this.state.password && this.state.password.length < 8) {
+            messageString = "Please Enter at Least 8 character"
+        }
+        else if (isValidPassword(this.state.password) === false) {
+            messageString = "Password must contain at least 8 characters including 1 upper case letter, 1 number and 1 special character."
+        }
+
+        if (messageString.length > 0) {
+            this.context.showAlert({ title: messageString })
+            return
+        }
+
+        this.context.presentActivityIndicator()
+        signUpUser({ name: this.state.username, email: this.state.email, password: this.state.password }).then((response) => {
+            this.context.dismissActivityIndicator()
+            if (response.error === null) {
+                User.shared = new User(response.object)
+                setCurrentUser({ email: this.state.email, password: this.state.password }).then(() => {
+                    this.props.navigation.navigate(Routes.AppNavigator);
+                })
+            } else {
+                this.context.showAlert({ title: "Error", message: response.error.message })
+            }
+        })
     }
 
     showPasswordButtonAction() {
@@ -58,8 +102,8 @@ export default class LoginView extends Component {
                 {this.renderUsernameTextInputView()}
                 {this.renderEmailTextInputView()}
                 {this.renderPasswordTextInputView()}
-                {this.renderLoginButton()}
                 {this.renderSignUpButton()}
+                {this.renderSignInButton()}
             </View>
         );
     }
@@ -119,7 +163,7 @@ export default class LoginView extends Component {
                         secureTextEntry={!(this.state.isShowingPassword)}
                         style={styles.passwordInputViewStyle}
                         onChangeText={(text) => this.setState({ password: text })}
-                        onSubmitEditing={(event) => { this.logInButtonAction() }}
+                        onSubmitEditing={(event) => { this.signUpButtonAction() }}
                     />
                     <TouchableHighlight onPress={() => { this.showPasswordButtonAction() }}
                         underlayColor={Color.themeBackground} style={styles.eyeButtonView}>
@@ -133,9 +177,9 @@ export default class LoginView extends Component {
         )
     }
 
-    renderLoginButton() {
+    renderSignUpButton() {
         return (
-            <TouchableHighlight onPress={() => { this.logInButtonAction() }}
+            <TouchableHighlight onPress={() => { this.signUpButtonAction() }}
                 underlayColor={Color.themeBackground}
                 style={styles.loginTouchContainer}>
                 <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={[Color.themeDark, Color.themeLight]} style={styles.logInButtonContainer}>
@@ -145,12 +189,12 @@ export default class LoginView extends Component {
         )
     }
 
-    renderSignUpButton() {
+    renderSignInButton() {
         return (
             <View style={styles.signupContainer}>
                 <Text style={styles.accountText}>{"Already have an acoount ?"}</Text>
                 <Text onPress={() => {
-                    this.signUpButtonAction()
+                    this.signInButtonAction()
                 }} style={styles.signUpText}>{"Sign in"}</Text>
             </View>
         )
