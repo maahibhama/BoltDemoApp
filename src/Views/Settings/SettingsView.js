9962b783d03dc2c1ee285746f58f9693270c74b2
Mahendra Bhama <maahibhama@gmail.com>
2019-01-21 15:02:08 +0530

    Update Setting screen with api calls

--- src/Views/Settings/SettingsView.js
+++ src/Views/Settings/SettingsView.js
@@ -1,28 +1,157 @@
 import React, { Component } from 'react';
 import { StyleSheet, SafeAreaView, Text, View, TouchableHighlight, Switch, Image, AsyncStorage } from 'react-native';
+import PropTypes from "prop-types";
+import { NavigationEvents } from 'react-navigation';
+
 import MenuNavigationHeader from '../../CustomUI/navigation-header/MenuNavigationHeader';
 import { ScrollView } from 'react-native-gesture-handler';
-import { NavigationActions } from "react-navigation";
+import { StackActions, NavigationActions } from "react-navigation";
 import { Color } from '../../common/Colors';
 import { SettingsData, SettingType, isLoginUser } from '../../common/Constants';
 import { AppFont } from '../../common/Fonts';
 import { Icons } from '../../common/Assets';
 import Routes from '../../Navigation/Routes';
-
+import { changeUserService, getUserServiceDetails } from '../../API/APIController';
+import { User } from '../../Prototypes/User';
 export default class SettingsView extends Component {
     static navigationOptions = {
         header: null
     }
 
+    static contextTypes = {
+        presentActivityIndicator: PropTypes.func.isRequired,
+        dismissActivityIndicator: PropTypes.func.isRequired,
+        showAlert: PropTypes.func.isRequired
+    };
+
+    state = {
+        receiveNotification: false,
+        receiveNewsletters: false,
+        receiveSpecialOffer: false,
+        participateBetaProgramme: false
+    }
+
     logoutButtonAction() {
-        AsyncStorage.setItem(isLoginUser, "false").then(()=>{
+        AsyncStorage.clear().then(() => {
             this.props.navigation.dispatch(NavigationActions.navigate({ routeName: Routes.AuthLoading }))
         })
+    }
+
+    switchValue({ id }) {
+        switch (id) {
+            case "4":
+                return this.state.receiveNotification
+            case "5":
+                return this.state.receiveNewsletters
+            case "6":
+                return this.state.receiveSpecialOffer
+            case "7":
+                return this.state.participateBetaProgramme
+            default:
+                return
+        }
+    }
+
+    touchViewAction({ id }) {
+        switch (id) {
+            case "1":
+                this.resetNavigation()
+                this.props.navigation.navigate(Routes.ProfileNavigation)
+                return
+            case "2":
+                this.props.navigation.navigate(Routes.ChangePassword)
+                return
+            case "3":
+                this.resetNavigation()
+                this.props.navigation.navigate(Routes.LanguageNavigation)
+                return
+            default:
+                return
+        }
+    }
+
+    switchAction({ id }) {
+        switch (id) {
+            case "4":
+                this.setState({ receiveNotification: !this.state.receiveNotification }, () => {
+                    this.updateUserService()
+                })
+                return
+            case "5":
+                this.setState({ receiveNewsletters: !this.state.receiveNewsletters }, () => {
+                    this.updateUserService()
+                })
+                return
+            case "6":
+                this.setState({ receiveSpecialOffer: !this.state.receiveSpecialOffer }, () => {
+                    this.updateUserService()
+                })
+                return
+            case "7":
+                this.setState({ participateBetaProgramme: !this.state.participateBetaProgramme }, () => {
+                    this.updateUserService()
+                })
+                return
+            default:
+                return
+        }
+
+    }
+
+    updateUserService() {
+        this.context.presentActivityIndicator()
+        changeUserService({
+            id: User.shared.id,
+            receiveNotification: this.state.receiveNotification,
+            receiveNewsletters: this.state.receiveNewsletters,
+            receiveSpecialOffer: this.state.receiveSpecialOffer,
+            participateBetaProgramme: this.state.participateBetaProgramme,
+        }).then(response => {
+            this.context.dismissActivityIndicator()
+            if (response.error === null) {
+                this.setState({
+                    receiveNotification: response.object.receiveNotification,
+                    receiveNewsletters: response.object.receiveNewsletters,
+                    receiveSpecialOffer: response.object.receiveSpecialOffer,
+                    participateBetaProgramme: response.object.participateBetaProgramme,
+                })
+            } else {
+                this.context.showAlert({ title: "Error", message: response.error.message })
+            }
+
+        })
+    }
+
+    fetchUserServiceDetails() {
+        getUserServiceDetails({ id: User.shared.id }).then(response => {
+            if (response.error === null) {
+                this.setState({
+                    receiveNotification: response.object.receiveNotification,
+                    receiveNewsletters: response.object.receiveNewsletters,
+                    receiveSpecialOffer: response.object.receiveSpecialOffer,
+                    participateBetaProgramme: response.object.participateBetaProgramme,
+                })
+            } else {
+                this.context.showAlert({ title: "Error", message: response.error.message })
+            }
+        })
+    }
+
+    resetNavigation() {
+        this.props.navigation.dispatch(StackActions.reset({
+            index: 0,
+            actions: [
+                NavigationActions.navigate({
+                    routeName: Routes.Settings,
+                })
+            ],
+        }));
     }
 
     render() {
         return (
             <SafeAreaView style={styles.container}>
+                <NavigationEvents onDidFocus={() => this.fetchUserServiceDetails()} />
                 <MenuNavigationHeader navigation={this.props.navigation} title={"Settings"} />
                 <ScrollView>
                     {this.renderSettingView()}
@@ -38,16 +167,25 @@ export default class SettingsView extends Component {
                 return (
                     <View key={rowValue.id} style={styles.switchViewStyle}>
                         <Text style={styles.switchText}>{rowValue.name}</Text>
-                        <Switch />
+                        <Switch
+                            onValueChange={() => { this.switchAction({ id: rowValue.id }) }}
+                            value={this.switchValue({ id: rowValue.id })}
+                            thumbColor={Color.themeDark}
+                            trackColor={{ false: Color.brightBorder, true: Color.themeLight }}
+                        />
                     </View>
                 )
             }
 
             return (
-                <View key={rowValue.id} style={styles.switchViewStyle}>
-                    <Text style={styles.normalText}>{rowValue.name}</Text>
-                    <Image source={Icons.arrow} />
-                </View>
+                <TouchableHighlight onPress={() => { this.touchViewAction({ id: rowValue.id }) }}
+                    underlayColor={Color.themeBackground}
+                    key={rowValue.id}>
+                    <View key={rowValue.id} style={styles.switchViewStyle}>
+                        <Text style={styles.normalText}>{rowValue.name}</Text>
+                        <Image source={Icons.arrow} />
+                    </View>
+                </TouchableHighlight>
             )
         })
     }
@@ -87,9 +225,9 @@ const styles = StyleSheet.create(
         },
         normalText: {
             flex: 1,
-            fontSize: AppFont.titleLargeBold.size,
-            fontFamily: AppFont.titleLargeBold.name,
-            fontWeight: AppFont.titleLargeBold.weight,
+            fontSize: AppFont.titleLargeMedium.size,
+            fontFamily: AppFont.titleLargeMedium.name,
+            fontWeight: AppFont.titleLargeMedium.weight,
             color: Color.darkText
         },
         logOutView: {
@@ -100,7 +238,7 @@ const styles = StyleSheet.create(
             justifyContent: "center",
             alignItems: "center"
         },
-        logoutText:{
+        logoutText: {
             fontSize: AppFont.titleMedium.size,
             fontFamily: AppFont.titleMedium.name,
             fontWeight: AppFont.titleMedium.weight,
